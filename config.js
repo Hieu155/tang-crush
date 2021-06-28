7856991ed9f248550391f0b21a6cdf29dd454b33
Hieu155 <73087531+Hieu155@users.noreply.github.com>
2021-06-28 16:40:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay call hát nhó <3 ',
+    messDesc: 'Ib yêu anh luôn đi chứ còn gì nữa <3 yêu em ',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100055690565080' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
