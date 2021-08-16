727cc0f83356639fa3b8943f6199736b47d6ac71
Hieu155 <73087531+Hieu155@users.noreply.github.com>
2021-08-16 07:59:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Linh à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Ib yêu anh luôn đi chứ còn gì nữa <3 yêu em ',
+    introTitle: 'Hí Pinky!',
+    introDesc: `Yêu anh vui vẻ Hồng cười
+Má hồng môi đỏ nhìn tươi lạ lùng
+Ước gì Hồng hóa thành vung
+Anh là nồi úp mình chung một lò.`,
+    btnIntro: 'OKE Bạn nhó <3',
+    title: 'Phải chăng bạn đã thích mình ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng bạn đã say ngay từ lúc thấy nụ cười ấy',
+    btnYes: 'Mãi bên nhau bạn nhó <3',
+    btnNo: 'Không, next đi conchongu :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại thích mình <3',
+    btnReply: 'send',
+    reply: 'Mãi bên nhau bạn nhó <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Ib yêumình luôn đi chứ còn gì nữa Pinky <3 ',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/100055690565080' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
